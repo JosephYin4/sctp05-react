109f1eee6719ffdf44241c6ccf1b12a72dc442d9
JosephYin4 Yin <yincc147@hotmail.com>
2024-10-25 06:44:29 +0000

    minor updates

--- 01-react-basics/src/App.jsx
+++ 01-react-basics/src/App.jsx
@@ -1,27 +1,20 @@
-// React uses an alternative method to `require`
-// for external modules
-// We use import 
-// the line below is the same as: const react = require('React')
 import React from 'react';
-import './App.css';
+import './styles.css';
 
-/* This is the starting point of all React applications */
 function App() {
-  // The App function must always return JSX
-  // RULES FOR JSX
-  // 1. Only one top level element
-  //  - usually we use a React Fragment <> ... </>
-  // 2. No syntax error is allowed
-  // 3. We cannot use `class` to attach a CSS class, we must
-  // use `className`
-  return (<>
-  <div className="container">
-    <h1>Sakura Japanese Resturant</h1>
-    <h2>Any more authentic you will have to fly to Japan</h2>
-    <p className="leading">We have the best authetnic and freshest ingredients</p>
-    </div>
-    </>)
-  ;
+  return(
+  <React.Fragment>
+  <h1>Hello World</h1>
+  <p>Welcome to our first React App</p>
+  <img src={require('./bubbletea.jpg')} alt="bubble tea" />
+  <img src={require('./vite.svg')} alt="vite logo"/>
+  {/* <img src={require('./vite.svg').default} alt="vite"/>
+  <img src={require('./bubbletea.jpg')} alt="bubble tea"/> */}
+  </React.Fragment>
+  );
 }
 
 export default App;
+
+
+
