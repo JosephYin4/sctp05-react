3fcea670064fdbc94cfcceb80db45a2997c39e51
JosephYin4 Yin <yincc147@hotmail.com>
2024-10-28 08:02:12 +0000

    minor updates

--- 01-basics/src/App.js
+++ 01-basics/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import "logo" from "./logo.svg"; // replace logo.svg with the file name of your
-//upload image, after making sure it's in the /src folder.
+import logo from "./logo.svg"; // replace logo.svg with the file name of your upload image, after making sure it's in the /src folder.
+import bubbletea from './bubbletea.jpg';
 
 function App() {
 return (
     <React.Fragment>
     <h1>Hello World</h1>
     <p>Welcome to our first React App</p>
-    <img src={logo}/>
+    <img src={logo} alt="logo.svg"/>
+    <img src={bubbletea} alt="bubbletea.jpg"/>
     </React.Fragment>
   );
 }
