4652c55a457317a35c0e8f42e49a87e8f15b7ce7
JosephYin4 Yin <yincc147@hotmail.com>
2024-10-28 10:35:29 +0000

    minor updates

--- 01-basics/src/App.js
+++ 01-basics/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css';
 import logo from "./logo.svg"; // replace logo.svg with the file name of your upload image, after making sure it's in the /src folder.
 import bubbletea from './bubbletea.jpg';
 
@@ -6,8 +7,8 @@ function App() {
 return (
     <React.Fragment>
     <h1>Hello World</h1>
-    <p>Welcome to our first React App</p>
-    <img src={logo} alt="logo.svg"/>
+    <p style={{backgroundColor:'yellow'}}>Welcome to our first React App</p>
+    <img src={logo} alt="logo.svg" width="300" height="300"/>
     <img src={bubbletea} alt="bubbletea.jpg"/>
     </React.Fragment>
   );
